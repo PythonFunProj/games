4b4b6d54f1a0119f223585c49ecb5a5033bd34dd
NicolasTraglia <81154021+NicolasTraglia@users.noreply.github.com>
2021-04-05 17:20:35 +0200

    Button
    added a quit button

--- Menu.py
+++ Menu.py
@@ -1,6 +1,7 @@
 ##PythonFun##
 
-#To do: fix the background
+#To do: turn the button creation into a function for more efficiency
+#       find a way to prevent the button from working when we are already clicking then gliding on the button
 
 import pygame
 import random
@@ -20,6 +21,10 @@ mainclock = pygame.time.Clock()
 font = pygame.font.SysFont(None,20)
 run = True
 background = pygame.image.load('backgroundmainmenu.png')
+green = (0,200,0)
+bright_green = (0,255,0)
+red = (200,0,0)
+bright_red = (255,0,0)
 
 
 def draw_text(text, font, color, surface, x, y): #drawtext function
@@ -31,16 +36,35 @@ def draw_text(text, font, color, surface, x, y): #drawtext function
 def main_menu(): #mainmenu function
     pygame.event.get()
     while run:
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        action = click[0]
         screen.fill((0,0,0))
         screen.blit(background,[0,0])
         draw_text('-Main Menu-', font, (255,0,0), screen, 210,20)
-        pygame.display.update()
-        mainclock.tick(20)
+        draw_text(str(mouse), font, (0,255,0),screen, 0, 0)
+        
+        if 250+50 > mouse[0] > 250-50 and 700 > mouse[1] > 700-25:
+            pygame.draw.rect(screen, bright_red,(200,675,100,25))
+            draw_text("QUIT", font, (50,50,50), screen, 235, 680)
+            if click[0] == 1:
+                pygame.quit()
+                exit()
+
+        else:
+            pygame.draw.rect(screen, red,(200,675,100,25))
+            draw_text("QUIT", font, (0,0,0), screen, 235, 680)
+        
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
-                    
+
+        
+
+        pygame.display.update()
+        mainclock.tick(15)
 
 main_menu()
 pygame.quit()
